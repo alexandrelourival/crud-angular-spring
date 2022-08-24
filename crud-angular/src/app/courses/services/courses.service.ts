a90a3c90bf2c1ded6a37d358089c2e3b2f158adb
alexandrelourival <lourivalalee@gmail.com>
2022-08-24 16:53:53 -0300

    Class 20

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from './../model/course';
-import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +15,14 @@ export class CoursesService {
 
   list() {
     return this.httpClient.get<Course[]>(this.API)
-    .pipe(
-      first(),
-      delay(5000),
-      tap( courses => console.log(courses))
-    );
+      .pipe(
+        first(),
+        delay(5000),
+        tap(courses => console.log(courses))
+      );
+  }
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
